023430d162d54ceb5f3d1c980587cffc714b6cd6
BotSMP3 <109872054+BotSMP3@users.noreply.github.com>
2022-09-03 16:38:14 +0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,35 @@
+global.DeveloperMode = 'false' //true Or false
+global.linkGC = ['https://chat.whatsapp.com/DmYC7VwLxpVIPHrysJFIC7']
+global.owner = ['6282114499086', '6282114499086', '6282114499086']// Put your number here
+global.mods = [] // Want some help?
+global.prems = [] // Premium user has unlimited limit
+global.APIs = { // API Prefix
+  // name: 'https://website'
+  nrtm: 'https://nurutomo.herokuapp.com',
+  xteam: 'https://api.xteam.xyz',
+  nzcha: 'http://nzcha-apii.herokuapp.com',
+  bg: 'http://bochil.ddns.net',
+  fdci: 'https://api.fdci.se',
+  bsbt: 'https://bsbt-api-rest.herokuapp.com'
+}
+global.APIKeys = { // APIKey Here
+  // 'https://website': 'apikey'
+  'https://api.xteam.xyz': 'd90a9e986e18778b',
+  'https://bsbt-api-rest.herokuapp.com': 'benniismael'
+}
+
+// Sticker WM
+global.packname = 'sergio ganteng\'sergio'
+global.author = 'sergio'
+
+global.multiplier = 69 // The higher, The harder levelup
+
+let fs = require('fs')
+let chalk = require('chalk')
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+  fs.unwatchFile(file)
+  console.log(chalk.redBright("Update 'config.js'"))
+  delete require.cache[file]
+  require(file)
+})
